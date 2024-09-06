6769956cde6181742bea9028318cabc343ea4470
melloCoding <mrjeffreymello@gmail.com>
2024-09-05 22:07:43 -0400

    modified:   nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  css: ['~/assets/css/tailwind.css']
 })
