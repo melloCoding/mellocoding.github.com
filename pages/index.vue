60df52693a1fa369009465cdb84f68eec15efd65
melloCoding <mrjeffreymello@gmail.com>
2024-09-07 21:25:48 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.vue 	modified:   tailwind.config.js

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
     <div>
      <h1 class="bg-slate-500 rounded-md">Welcome to the index.vue</h1>
     </div>
+    <button class="btn">hello there</button>
     <ul>
      <li><NuxtLink to="/gamedev">index page</NuxtLink></li>
     </ul>
