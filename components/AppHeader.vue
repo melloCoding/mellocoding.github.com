1f122e73ff09ba24154e744fa70058a603800ad2
melloCoding <mrjeffreymello@gmail.com>
2024-09-10 21:11:15 -0400

    modified:   components/AppHeader.vue 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/index.vue 	new file:   public/default-favicon.ico 	modified:   public/favicon.ico

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,3 +1,7 @@
 <template>
-    <nav class="navbar bg-secondary"><NuxtLink to="/">Jeffrey Mello</NuxtLink></nav>
+    <nav class="navbar bg-secondary">
+        <NuxtLink to="/">
+            <div class="navbar-center"><div class="btn btn-ghost text-xl">Jeffrey Mello</div></div>
+        </NuxtLink>
+        </nav>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="h-screen bg-base-100">
+    <!-- Navbar -->
     <AppHeader />
-    <div>
-     <h1 class="bg-slate-500 rounded-md">Welcome to the index.vue</h1>
-    </div> 
+    <!-- Attempting to replicating something I found on the title page of beta.civilhost.net -->
+    <div class="h-auto md:flex flex-cols-2 py-16 justify-between relative">
+      <h1 class="self-center">Jeffreymello</h1>
+    </div>
     <NuxtLink to="/gamedev"><button class="btn">Game Dev page</button></NuxtLink>
   </div>
   </template>
