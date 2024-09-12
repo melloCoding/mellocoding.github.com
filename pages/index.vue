c8f92fc6d2a8a1832f485bedb74703b87accf0a8
melloCoding <61124766+melloCoding@users.noreply.github.com>
2024-09-12 08:44:56 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,8 @@
     <AppHeader />
     <!-- Attempting to replicating something I found on the title page of beta.civilhost.net -->
     <div class="card place-items-center bg-base-300">
-      <h1 class="">Jeffreymello</h1>
+      <h1 class="items-start">Jeffreymello</h1>
+      <img class="items-end" href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBliss_%2528photograph%2529&psig=AOvVaw3vRZFr9fOqlWArukJ8jjEi&ust=1726231453253000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLCvsLa3vYgDFQAAAAAdAAAAABAE">
     </div>
     <NuxtLink to="/gamedev"><button class="btn">Game Dev page</button></NuxtLink>
   </div>
