a914a9654849f08ce397592762609ea58abb68c1
melloCoding <61124766+melloCoding@users.noreply.github.com>
2024-09-12 08:24:44 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
     <!-- Navbar -->
     <AppHeader />
     <!-- Attempting to replicating something I found on the title page of beta.civilhost.net -->
-    <div class="h-auto md:flex flex-cols-2 py-16 justify-between relative">
-      <h1 class="self-center">Jeffreymello</h1>
+    <div class="card place-items-center">
+      <h1 class="">Jeffreymello</h1>
     </div>
     <NuxtLink to="/gamedev"><button class="btn">Game Dev page</button></NuxtLink>
   </div>
