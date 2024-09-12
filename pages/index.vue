56716c900864fc914aa23df8a33f989df4f1c705
melloCoding <61124766+melloCoding@users.noreply.github.com>
2024-09-12 08:26:10 -0400

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <!-- Navbar -->
     <AppHeader />
     <!-- Attempting to replicating something I found on the title page of beta.civilhost.net -->
-    <div class="card place-items-center">
+    <div class="card place-items-center bg-base-300">
       <h1 class="">Jeffreymello</h1>
     </div>
     <NuxtLink to="/gamedev"><button class="btn">Game Dev page</button></NuxtLink>
