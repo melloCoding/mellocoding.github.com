f29510b015fd164fe6b1e1672649140c88b5399e
melloCoding <mrjeffreymello@gmail.com>
2024-09-13 20:50:52 -0400

    modified:   pages/index.vue 	modified:   tailwind.config.js

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,10 @@
     <!-- Navbar -->
     <AppHeader />
     <!-- Attempting to replicating something I found on the title page of beta.civilhost.net -->
-    <div class="card place-items-center bg-base-300">
+    <div class="card place-items-center bg-base-100">
       <h1 class="items-start">Jeffreymello</h1>
-      <img class="items-end" href="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FBliss_%2528photograph%2529&psig=AOvVaw3vRZFr9fOqlWArukJ8jjEi&ust=1726231453253000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCLCvsLa3vYgDFQAAAAAdAAAAABAE">
+      <!-- Place Holder Image CHANGE BEOFRE DONE WITH LIKE YOUR FISH OR SOMETHING-->
+      <img class="items-end" src="https://cdn.mos.cms.futurecdn.net/iC7HBvohbJqExqvbKcV3pP-1200-80.jpg">
     </div>
     <NuxtLink to="/gamedev"><button class="btn">Game Dev page</button></NuxtLink>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./"],
+  content: [
+    "./",
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.{vue,js,ts}",
+    "./pages/**/*.{vue,js,ts}",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}"
+  ],
   theme: {
     extend: {},
   },
@@ -8,24 +15,15 @@ export default {
     themes: [
       {
         mytheme: {
-          
-"primary": "#6b7280",
-          
-"secondary": "#4b5563",
-          
-"accent": "#f97316",
-          
-"neutral": "#565c67",
-          
-"base-100": "#374151",
-          
-"info": "#67e8f9",
-          
-"success": "#00ff00",
-          
-"warning": "#fbbf24",
-          
-"error": "#ff0000",
+          "primary": "#6b7280",
+          "secondary": "#4b5563",
+          "accent": "#f97316",
+          "neutral": "#565c67",
+          "base-100": "#374151",
+          "info": "#67e8f9",
+          "success": "#00ff00",
+          "warning": "#fbbf24",
+          "error": "#ff0000",
           },
         },
       ],
