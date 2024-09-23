aacf86eebb7e644530f2de54a8b5bb0269af1424
melloCoding <mrjeffreymello@gmail.com>
2024-09-23 07:35:45 -0400

    modified:   components/AppHeader.vue 	modified:   pages/index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
   <div>
     <AppHeader />
     <div
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 content-stretch px-[5%] gap-[5%]"
+      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-stretch px-[5%] gap-[5%]"
     >
       <ProjectCard
         title="Potatoes"
