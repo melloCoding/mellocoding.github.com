d55ba16cab2dbd06e2ab56bc01e98d32965c2384
melloCoding <mrjeffreymello@gmail.com>
2024-10-29 08:23:41 -0400

    modifyed appheader.vue

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,13 +2,13 @@
   <div class="navbar nav mb-5">
     <div class="flex-1">
       <NuxtLink to="/" class="btn btn-ghost text-xl">Jeffrey Mello</NuxtLink>
-    </div class="">
-    <div class="flex-none btn btn-ghost text-lg">
-      <NuxtLink to="/gamedev">Devlog</NuxtLink>
     </div>
-    <div class="flex-none btn btn-ghost text-lg">
-      <NuxtLink to="/robotics">Robotics</NuxtLink>
-    </div>
-    
+    <NuxtLink to="/gamedev">
+      <div class="flex-none btn btn-ghost text-lg">Devlog</div>
+    </NuxtLink>
+
+    <NuxtLink to="/robotics">
+      <div class="flex-none btn btn-ghost text-lg">Robotics</div>
+    </NuxtLink>
   </div>
 </template>
