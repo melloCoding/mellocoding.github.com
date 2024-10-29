01eb45bb6c7455662a094328b8e8008cee793864
melloCoding <mrjeffreymello@gmail.com>
2024-10-29 12:12:19 -0400

    updated robotics.vue

--- pages/robotics.vue
+++ pages/robotics.vue
@@ -2,7 +2,11 @@
   <div>
     <AppHeader />
     <h1 class="text-5xl font-bold text-center">Robotics page</h1>
-    <p>I am a member of the robotics team</p>
+    <p>
+      I am a member of the robotics team at Coral Springs Charter School. We
+      partcipate in the First Tech Challenge(FTC). We are team 13233 Cyber
+      Kinghts
+    </p>
   </div>
 </template>
 
