48758173774be8207bfa30d805177fddddca33ff
melloCoding <mrjeffreymello@gmail.com>
2024-10-29 12:15:44 -0400

    further updated robotics.vue

--- pages/robotics.vue
+++ pages/robotics.vue
@@ -5,7 +5,7 @@
     <p>
       I am a member of the robotics team at Coral Springs Charter School. We
       partcipate in the First Tech Challenge(FTC). We are team 13233 Cyber
-      Kinghts
+      Kinghts. The FTC Game for this year is Into The Deep.
     </p>
   </div>
 </template>
