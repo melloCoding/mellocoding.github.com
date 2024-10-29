3817ea88c643bc997360af154bace350289b3c95
melloCoding <mrjeffreymello@gmail.com>
2024-10-28 21:46:15 -0400

    created a 404 page and added contents to it

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="pt-10">
+    <p class="text-5xl text-center">Error 404</p>
+    <p class="text-center pt-5">
+      Oops Your not supposed to be here. Click the button to return to the
+      homepage.
+    </p>
+    <div class="flex justify-center pt-3">
+      <nuxtLink to="/"><div class="btn">Return Home</div></nuxtLink>
+    </div>
+  </div>
+</template>
