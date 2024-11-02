023de985905377a2d6251cc52cb979531676e344
melloCoding <mrjeffreymello@gmail.com>
2024-11-02 10:57:34 -0400

    edited the weekone.vue also added a photo to use for weekone.vue

--- pages/robotic/weekone.vue
+++ pages/robotic/weekone.vue
@@ -4,14 +4,27 @@
     <h1 class="text-5xl font-bold text-center mb-10">
       First Documented Build Log For the Into the Deep Season
     </h1>
-    <p class="px-10 text-xl">
-      This is the first actually log for our team's season. I'm a bit late to
-      this because i've been busy coding the rest of the website. So at the
-      momment we have a mostly functional robot. In the robots curent state it
-      can drive around and strafe. The robot is also capable of almost hanging
-      itself on the low bar of the truss. We have attached a linear slider to
-      the robot aswell to hopefully allow us to score once we have a working
-      claw.
-    </p>
+    <div class="flex px-10">
+      <p class="text-xl">
+        This is the first actually log for our team's season. I'm a bit late to
+        this because i've been busy coding the rest of the website. So at the
+        momment we have a mostly functional robot. In the robots curent state it
+        can drive around and strafe. The robot is also capable of almost hanging
+        itself on the low bar of the truss. We have attached a linear slider to
+        the robot aswell to hopefully allow us to score once we have a working
+        claw.
+      </p>
+      <img
+        class="w-[50%] rounded-xl"
+        src="/imgs/robotics/robotonfeildone.jpeg"
+        alt="aformentioned robot on the field"
+      />
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "Robotics Week One Log",
+});
+</script>
