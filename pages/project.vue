cea299c12946a47aa363f45a28d768ce844e3bf6
melloCoding <mrjeffreymello@gmail.com>
2025-01-11 15:27:06 -0500

    addded to project.vue created hitbox1.vue and added hitboxv1.jpeg

--- pages/project.vue
+++ pages/project.vue
@@ -2,4 +2,17 @@
   <AppHeader />
 
   <h1 class="text-5xl font-bold text-center mb-10">Projects</h1>
+  <div class="divider px-10"></div>
+  <div
+    class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 content-stretch px-[5%] gap-[5%] mb-10 item-center"
+  >
+    <!-- Edit the image source-->
+    <ProjectCard
+      title="Custom Hitbox Controller V1"
+      description="Custom made hitbox style controller for tekken"
+      imageSrc="/imgs/hitbox1.jpeg"
+      imgAltText="A 3d printed hitbox flat controller that is orange and white"
+      routeLocation="projects/hitboxv1"
+    />
+  </div>
 </template>
